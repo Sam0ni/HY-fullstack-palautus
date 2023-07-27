15fdaa287445317b4243c3a82e3d03d220098c86
Samppa Salo <samppasalo16@gmail.com>
2023-07-27 19:46:09 +0300

    add exercise 1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -39,24 +39,26 @@ const Part = (part) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total one={part1.exercises} two={part2.exercises} three={part3.exercises}/>
+      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]}/>
+      <Total one={parts[0].exercises} two={parts[1].exercises} three={parts[2].exercises}/>
     </div>
   )
 }
