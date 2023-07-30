99f9227c7b5e3c36a77308aa45f7355ca2a3e63d
Samppa Salo <samppasalo16@gmail.com>
2023-07-30 22:59:10 +0300

    add exercise 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -15,21 +15,47 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAvg] = useState(0)
+  const [positive, setPos] = useState(0)
 
-  const increaseValue = (value, setter) => () => {
-    setter(value + 1)
+  const increaseGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+    setAvg((updatedGood - bad)/ (updatedGood + neutral + bad))
+    setPos((updatedGood / (updatedGood + neutral + bad)))
   }
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+    setAvg((good - bad)/ (good + updatedNeutral + bad))
+    setPos((good / (good + updatedNeutral + bad)))
+  }
+  const increaseBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(updatedBad + good + neutral)
+    setAvg((good - updatedBad)/ (good + neutral + updatedBad))
+    setPos((good / (good + neutral + updatedBad)))
+  }
+
+
 
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick={increaseValue(good, setGood)} text={"Good"}/>
-      <Button handleClick={increaseValue(neutral, setNeutral)} text={"Neutral"}/>
-      <Button handleClick={increaseValue(bad, setBad)} text={"Bad"}/>
+      <Button handleClick={increaseGood} text={"Good"}/>
+      <Button handleClick={increaseNeutral} text={"Neutral"}/>
+      <Button handleClick={increaseBad} text={"Bad"}/>
       <h1>Statistics</h1>
       <Display value={good} text={"Good"}/>
       <Display value={neutral} text={"Neutral"}/>
       <Display value={bad} text={"Bad"}/>
+      <Display value={total} text={"All"}/>
+      <Display value={average} text={"Average"}/>
+      <Display value={positive + " %"} text={"Positive"}/>
     </div>
   )
 }
