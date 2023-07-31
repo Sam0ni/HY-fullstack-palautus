80ec6388ef03bdc3c330ee481217d0bc674a5978
Samppa Salo <samppasalo16@gmail.com>
2023-08-01 00:00:42 +0300

    add exercise 2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,12 +8,14 @@ const Display = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "040-1233210"
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
+  const [filterStr, setFilterStr] = useState("")
 
   const addNewName = (event) => {
     event.preventDefault()
@@ -44,9 +46,29 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setFilterStr(event.target.value)
+  }
+
+  const filterPhoneBook = () => {
+    return (
+      persons.filter(person => person.name.toLowerCase().includes(filterStr.toLowerCase()))
+    )
+  }
+
+  const peopleToDispay = filterStr === ""
+    ? persons
+    : filterPhoneBook()
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter
+      <input
+      value={filterStr} 
+      onChange={handleFilterChange}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addNewName}>
         <div>
           name: 
@@ -66,7 +88,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
 
-      <Display persons={persons}/>
+      <Display persons={peopleToDispay}/>
     </div>
   )
 
