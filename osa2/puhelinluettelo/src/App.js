4441747a1a23b5dbb6d4cbcd585b0466f1436eb0
Samppa Salo <samppasalo16@gmail.com>
2023-08-01 15:02:13 +0300

    add exercise 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Display = ({persons}) => {
   return(
@@ -39,15 +40,16 @@ const FilterNames = ({filterStr, handleFilterChange}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
   const [filterStr, setFilterStr] = useState("")
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response => setPersons(response.data))
+  }, [])
 
   const addNewName = (event) => {
     event.preventDefault()
