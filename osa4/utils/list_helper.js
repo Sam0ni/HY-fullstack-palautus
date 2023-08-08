7f3252a597a922399d53c9a09a20d3aa95da7d3b
Samppa Salo <samppasalo16@gmail.com>
2023-08-08 19:03:11 +0300

    add exercises 4.5 4.6

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require("lodash")
+
 const dummy = (blogs) => {
     return 1
   }
@@ -6,7 +8,38 @@ const totalLikes = (blogs) => {
     return blogs.reduce(((sum, currentVal) => sum + currentVal.likes), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = blogs[0]
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog
+    }
+  });
+  retFavorite = {...favorite}
+  delete retFavorite._id
+  delete retFavorite.__v
+  delete retFavorite.url
+  return retFavorite
+}
+
+const mostBlogs = (blogs) => {
+  const authors = blogs.map(blog => blog.author)
+  const counted = Object.entries(lodash.countBy(authors))
+  let author = counted[0]
+  counted.forEach(count => {
+    if (count[1] > author[1]) {
+      author = count
+    }
+  })
+  return {
+    author:author[0],
+    blogs:author[1]
+  }
+}
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
   }
